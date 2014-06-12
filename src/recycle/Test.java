dbd656f8f6304e11419cd5ca74beaa65479e944f
mitya <mitya@samsung>
2014-06-12 17:31:09 +0300

    version 30-may-14

--- src/recycle/Test.java
+++ src/recycle/Test.java
@@ -17,9 +17,8 @@ class T2 extends T1{
 public class Test {
 
 	public static void main(String[] args) {
-		
-		System.out.println(("ID_гюопнь б_цпсоою б_мюхлемнбюмхе б_рс б_люпйю б_ед_хглепемхъ"
-				+ "  б_мю_яйкюде б_йнло_хмтн б_дюрю_йнло").split("\\s+").length);
+		int i = 10;
+		System.out.println(Integer.toBinaryString(~i));
 
 	}
 
